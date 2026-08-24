2b3b552d8691b00c0f3219ce8c32f3607eeeffe4
Z User <z@container>
2026-08-24 10:08:36 +0000

    Update Turso auth token

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import { createClient, type Client } from '@libsql/client'
 
 const db: Client = createClient({
   url: 'libsql://tims-cake-db-shaheersohail284-gif.aws-ap-south-1.turso.io',
-  authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoiYmM0NmVkZGYtN2RlMi00ODRkLTlmNjQtM2Q0NzdkZGQ2OGQ2IiwiaWQiOiI3MmRkYWM5ZS01YmY5LTRmNTMtOTBlOC03ZjQyZTkzMTU4MmUiLCJyIjoiYXAtc291dGgtMSJ9.HtMIWLBdJqGyvV4xdR9ZwFSY6Y3p1jwIp1kNGE3KGzUlb6IVDHwA2m6s1ZGxiXD6HHYALHGJk3UeX2B-UMfrACg',
+  authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODc1NjU5MDksImlkIjoiMDFhMDJmNmEtNzAwMS03ZDhlLTg3NzEtZjBlMjhkZmQ3MjgxIiwia2lkIjoibWx1THVwYS1ZVVRpTWJhWGlDV1Y4Tl9OYzZ2NzVhV1JaY28taFdfM3phYyIsInJpZCI6ImJhYjE3M2M3LWI0MzctNDUzMy05NmZhLWZmNzIxMjlkODNmNyJ9.OVYuH4oDqm6d91cDn9nkZVXlFhfVxtY9hekKGwwNiMia-vthWs-dAVssoK4-lSBYAIAELRQtSEqtNCtsI-xAAA',
 })
 
 export { db, type Client }
